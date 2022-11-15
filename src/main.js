77124cfed1cf97abfb969acbfa697bc36b743445
李新 <763819849@qq.com>
2022-11-15 14:18:37 +0800

    feat:基本框架

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
+// 导入NProgress, 包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import './plugins/element.js'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+// 配置请求根路径
+axios.defaults.baseURL = 'http://42.193.22.5:8887/api/private/v1/'
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
 
+// 在request 拦截器中, 展示进度条 NProgress.start()
+// 请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use(config => {
+    NProgress.start()
+    // console.log(config)
+    // 为请求头对象，添加token验证的Authorization字段
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    // 在最后必须 return config
+    return config
+})
+// response 拦截器中,  隐藏进度条NProgress.done()
+axios.interceptors.response.use(config => {
+    NProgress.done()
+    return config
+})
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
---
+++ src/plugins/element.js
@@ -0,0 +1,85 @@
+import Vue from 'vue'
+import {
+    Alert,
+    Aside,
+    Breadcrumb,
+    BreadcrumbItem,
+    Button,
+    Card,
+    Cascader,
+    Checkbox,
+    CheckboxGroup,
+    Col,
+    Container,
+    Dialog,
+    Form,
+    FormItem,
+    Header,
+    Input,
+    Main,
+    Menu,
+    MenuItem,
+    MenuItemGroup,
+    Message,
+    MessageBox,
+    Option,
+    Pagination,
+    Row,
+    Select,
+    Step,
+    Steps,
+    Submenu,
+    Switch,
+    Table,
+    TableColumn,
+    TabPane,
+    Tabs,
+    Tag,
+    Timeline,
+    TimelineItem,
+    Tooltip,
+    Tree,
+    Upload
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 将弹框组件全局挂在到VUe原形实例
+Vue.prototype.$message = Message
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Step)
+Vue.use(Steps)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
